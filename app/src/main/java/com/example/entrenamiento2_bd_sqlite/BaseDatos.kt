2d191a8518fed4432ae6ce813c8474ed2d9c5301
EvaYuju <elopezmarquez@safareyes.es>
2023-02-11 01:33:39 +0100

    ALL OK

--- app/src/main/java/com/example/entrenamiento2_bd_sqlite/BaseDatos.kt
+++ app/src/main/java/com/example/entrenamiento2_bd_sqlite/BaseDatos.kt
@@ -1,5 +1,6 @@
 package com.example.entrenamiento2_bd_sqlite
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -19,7 +20,71 @@ class BaseDatos(contexto: Context):SQLiteOpenHelper(contexto, BD,null,1) {
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
     }
+    // CRUD
+    // INSERT
+    fun insertarDatos(usuario: Usuario):String{
+        val db = this.writableDatabase // prepara la base de datos editable
+        var contenedordeValores = ContentValues();
+        // put clave-valor:
+        contenedordeValores.put("nombre", usuario.nombre)
+        contenedordeValores.put("edad", usuario.edad)
 
+        var resultado=db.insert("usuario", null, contenedordeValores)
+        // si el resultado fue igual -1 hubo fallo
+       if(resultado==-1.toLong()) {
+           return "fallo al insertar";
+       } else {
+           return "Datos insertados (ok)"
+       }
+    }//insertarDatos
 
+    // READ
+    fun traerDatos():MutableList<Usuario>{
+        var lista:MutableList<Usuario> = ArrayList()
+        val db = this.readableDatabase
+        val sql = "SELECT * FROM Usuario";
+        var resultado = db.rawQuery(sql, null)
+        if(resultado.moveToFirst()){
+            do {
+                var usuario=Usuario()
+                usuario.id = resultado.getString(resultado.getColumnIndexOrThrow("id")).toInt()
+                usuario.nombre = resultado.getString(resultado.getColumnIndexOrThrow("nombre"))
+                usuario.edad = resultado.getString(resultado.getColumnIndexOrThrow("edad")).toInt()
+                lista.add(usuario)
+            } while (resultado.moveToNext())
+            resultado.close()
+            db.close()
+         return (lista)
+        }//if
+    return  lista
+    }//traerDatos
 
-}
+    // UPDATE
+    fun actualizaDatos(id:String, nombre:String, edad:Int):String {
+        val db = this.writableDatabase
+        var contenedor = ContentValues();
+         contenedor.put("nombre", nombre)
+         contenedor.put("edad", edad)
+         var resultado = db.update("Usuario", contenedor, "id=", arrayOf(id))
+        if (resultado > 0) {
+            // Validar la actualización
+            return "Actualización realizada"
+        } else {
+            //No se pudo actualizar
+            return "No se pudo actualizar"
+        }
+    }//actualizaDatos
+
+    // DELETE
+    fun borrarDatos(id:String){
+        val db = this.writableDatabase
+        if(id.length>0){
+            db.delete("Usuario","id=?", arrayOf(id))
+            // para borrar todos
+            // db.delete("Usuaro", null, null)
+            db.close()
+        }
+
+    }//borrarDatos
+
+}//class BaseDatos
--- app/src/main/java/com/example/entrenamiento2_bd_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/entrenamiento2_bd_sqlite/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.entrenamiento2_bd_sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Intents.Insert
+import android.view.View
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import com.example.entrenamiento2_bd_sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var binding: ActivityMainBinding
+    lateinit var nombre:EditText
+    lateinit var edad:EditText
+    lateinit var codigo:EditText
+    lateinit var mensaje:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nombre = findViewById(R.id.txtViewNombre)
+        edad = findViewById(R.id.textViewEdad)
+        codigo = findViewById(R.id.textViewID)
+        mensaje = findViewById(R.id.Lista)
     }
+    // Botón insertar
+    fun Insert(view:View) {
+        if(nombre.text.toString().length>0 && edad.text.toString().length>0) {
+            var usuario = Usuario(nombre.text.toString(), edad.text.toString().toInt());
+            var bd =BaseDatos(this);
+            mensaje.setText(bd.insertarDatos(usuario));
+        }
+    }
+
+    // Botón borrar
+    fun Delete(view: View) {
+        var bd = BaseDatos(this);
+        if(codigo.text.toString().length>0){
+        bd.borrarDatos(codigo.text.toString());
+        }
+    }
+
+    // Botón leer
+    fun Read(view: View) {
+        var bd=BaseDatos(this);
+        var datos = bd.traerDatos()
+        mensaje.text="";
+        for(i in 0..datos.size-1){
+                mensaje.append("Código" + datos.get(i).id.toString() + " Nombre "+ datos.get(i).nombre + "edad"+ datos.get(i).edad.toString())
+            }
+    }
+
+    // Botón actualizar
+    fun Update(view: View) {
+        var bd=BaseDatos(this);
+        if(codigo.text.toString().length>0 && edad.text.toString().length>0 && codigo.text.toString().length>0){
+            bd.actualizaDatos(codigo.text.toString(),nombre.text.toString(),edad.text.toString().toInt())
+        }
+    }
+
+
+
+
 }
